0dc3430a0147efaa39c6f43942a8ee4a7596d7d0
JaCk <115971931+Suyash1608@users.noreply.github.com>
2024-02-20 14:07:50 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (28.43%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -7,25 +7,31 @@ public:
         int l = 0, r = m - 1, t = 0, b = n - 1;
         vector<int> result;
         while (l <= r && t <= b) {
-            for (int i = l; i <= r; i++) {
+            for (int i = l; i <= r; i++) 
+            {
                 result.push_back(matrix[t][i]);
             }
             t++;
             
-            for (int i = t; i <= b; i++) {
+            for (int i = t; i <= b; i++) 
+            {
                 result.push_back(matrix[i][r]);
             }
             r--;
             
-            if (t <= b) {
-                for (int i = r; i >= l; i--) {
+            if (t <= b) 
+            {
+                for (int i = r; i >= l; i--) 
+                {
                     result.push_back(matrix[b][i]);
                 }
                 b--;
             }
             
-            if (l <= r) {
-                for (int i = b; i >= t; i--) {
+            if (l <= r) 
+            {
+                for (int i = b; i >= t; i--) 
+                {
                     result.push_back(matrix[i][l]);
                 }
                 l++;
