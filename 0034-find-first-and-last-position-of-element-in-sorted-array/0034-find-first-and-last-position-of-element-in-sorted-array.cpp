8bacfdfc6ae1d55309a5f636a0679835fd3f5252
JaCk <115971931+Suyash1608@users.noreply.github.com>
2024-02-27 01:00:33 +0530

    Time: 3 ms (90.90%), Space: 15.9 MB (54.85%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,33 +2,39 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int n = nums.size();
-        int start = -1; // initialize start to -1 instead of n
-        int end = -1; // initialize end to -1 instead of 0
+        int start = -1; 
+        int end = -1; 
         int l = 0;
         int h = n - 1;
-        while (l <= h) {
+        while (l <= h) 
+        {
             int mid = (l + h) / 2;
-            if (nums[mid] >= target) {
+            if (nums[mid] >= target) 
+            {
                 start = mid;
                 h = mid - 1;
             }
-            else {
+            else 
+            {
                 l = mid + 1;
             }
         }
         l = 0;
         h = n - 1;
-        while (l <= h) {
+        while (l <= h) 
+        {
             int mid = (l + h) / 2;
-            if (nums[mid] <= target) { // change the comparison to <= instead of >
+            if (nums[mid] <= target) 
+            { 
                 end = mid;
                 l = mid + 1;
             }
-            else {
+            else 
+            {
                 h = mid - 1;
             }
         }
-        if (start == -1 || nums[start] != target) // check if start is -1 or nums[start] is not equal to target
+        if (start == -1 || nums[start] != target) 
             return {-1, -1};
         return {start, end};
     }
