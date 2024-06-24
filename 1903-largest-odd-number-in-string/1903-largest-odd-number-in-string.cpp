10906f080c4c831df4580ed0d2f9ad2a45874e67
JaCk <115971931+Suyash1608@users.noreply.github.com>
2024-06-24 10:46:04 +0530

    Time: 32 ms (26.68%), Space: 16.3 MB (23.51%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -13,10 +13,10 @@ public:
         //     }
         // }
         // return out;
-         int n = num.size();
-        for (int i = n - 1; i >= 0; --i) {
-            if ((num[i] - '0') % 2 != 0) {
-                return num.substr(0, i + 1);
+         int n=num.size();
+        for(int i=n-1;i>=0;--i) {
+            if((num[i]-'0')%2!=0) {
+                return num.substr(0,i+1);
             }
         }
         return "";
