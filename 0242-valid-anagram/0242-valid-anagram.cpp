3456a7c9953a63fc87645d71d3626ff73dc056cd
JaCk <115971931+Suyash1608@users.noreply.github.com>
2024-06-29 10:20:41 +0530

    Time: 10 ms (45.79%), Space: 8.4 MB (96.77%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char,int> mps;
-        map<char,int> mpt;
-        if(s.length()!=t.length())
-            return false;
-        for(int i=0;i<s.length();i++)
-        {
-            mps[s[i]]++;
-            mpt[t[i]]++;
-        }
-       return mps==mpt;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
     }
 };
