e76612cc96a415637e61d45069e80aba2eea8f54
JaCk <115971931+Suyash1608@users.noreply.github.com>
2024-07-10 12:24:29 +0530

    Time: 3 ms (41.77%), Space: 7.5 MB (22.25%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -3,17 +3,14 @@ public:
     int maxDepth(string s) {
         int open=0;
         int result=0;
-        stack<char> ch;
         for(auto c : s)
         {
             result=max(result,open);
             if(c=='(')
             {
-                ch.push(c);
                 open++;
             }
             if(c==')'){
-                ch.pop();
                 open--;
             }
             
